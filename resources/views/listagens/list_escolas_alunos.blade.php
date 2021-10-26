653cee8679e68be88cd921d539b24ec62341ec0c
Cleison Freitas <89282274+cleis0n-dev@users.noreply.github.com>
2021-10-26 07:26:21 -0300

    Add files via upload

---
+++ resources/views/listagens/list_escolas_alunos.blade.php
@@ -0,0 +1,28 @@
+<div class="table-responsive">
+    <table class="table table-hover" id="dataTable" cellspacing="0" width="100%">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Aluno</th>
+                <th>Turma</th>
+                <th>Consultar</th>
+            </tr>
+        </thead>
+        <tbody>
+            @if ($matriculas)
+                @foreach ($matriculas as $matricula )
+                <tr>
+                    <td>{{ $matricula->id }}</td>
+                    <td>{{ $matricula->nome }}</td>
+                    <td>{{ $matricula->serie }}º {{ $matricula->nivel }} {{ $matricula->turno }} </td>
+                    <td><a href="{{ route('alunos.edit',$matricula->id_aluno) }}) }}" class="btn btn-dark btn-sm"><i class="far fa-edit"></i></a></td>
+                </tr>
+                @endforeach
+            @else
+            <tr>
+                <td colspan="4" style="text-align: center">Nenhum aluno matriculado</td>
+            </tr>
+            @endif
+        </tbody>
+    </table>
+</div>
